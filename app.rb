3ea8c458fdf1d8e6a8db544559b2f173465007d9
Nancerss <nancy.alquicirauc@gmail.com>
2018-07-31 16:22:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/Nancy'
+    "My name is Nancy"
+  end
+
+  # get '/Sai'
+
+
 end
